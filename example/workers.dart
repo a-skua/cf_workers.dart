0a9e470b6ac6294300aec6c5ba9742a8b0aaed0b
asuka <yagi.asuka.0x06@gmail.com>
2025-05-10 03:40:48 +0900

    feat: add workers.dart

--- example/workers.dart
+++ example/workers.dart
@@ -1,4 +1,10 @@
-import 'dart:js_interop';
 import 'package:cf_workers/workers.dart';
 
-main() => fetch((Request request) async => Response('Hello, Dart!'.toJS));
+main() => fetch(_handler, property: '__dart_fetch');
+
+Future<Response> _handler(Request request, Env env, Context context) async {
+  final assets = env['ASSETS'];
+  print('Assets: $assets');
+
+  return Response('Hello, Dart!'.toJS);
+}
--- lib/src/workers.dart
+++ lib/src/workers.dart
@@ -1,14 +1,19 @@
-import 'dart:js_interop';
-import 'dart:js_interop_unsafe';
 import 'package:poor_web/poor_web.dart';
-
 export 'package:poor_web/poor_web.dart';
 
-typedef Handler = Future<Response> Function(Request);
+typedef Env = Object;
+typedef Context = Object;
+
+typedef Handler = Future<Response> Function(Request, Env env, Context context);
 
 /// [fetch]
 ///
 /// ```dart
 /// void main() => fetch((Request r) async => Response('Hello, world!'.toJS));
-void fetch(Handler fetch, {String property = '__dart_fetch'}) => globalContext
-    .setProperty(property.toJS, ((Request r) => fetch(r).toJS).toJS);
+void fetch(Handler fetch, {String property = '__dart_fetch'}) {
+  JSPromise<Response> handle(Request r, JSObject e, JSObject c) {
+    return fetch(r, Env.fromJS(e), Context.fromJS(c)).toJS;
+  }
+
+  globalThis[property] = Object.fromJS(handle.toJS);
+}
